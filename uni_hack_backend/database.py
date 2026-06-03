77e535157b1e993c78f207c09f9b9e0e8883db28
Sam00-B <saumikbarua0@gmail.com>
2026-06-02 20:32:23 -0500

    Switched to ONNX local embeddings to save API tokens and RAM

--- uni_hack_backend/database.py
+++ uni_hack_backend/database.py
@@ -1,14 +1,12 @@
 import chromadb
 import uuid
-from chromadb.utils import embedding_functions
+import chromadb.utils.embedding_functions as embedding_functions
 
 # Initialize the Database
 client = chromadb.PersistentClient(path="./local_db")
 
 
-embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(
-    model_name="all-MiniLM-L6-v2"
-)
+embedding_fn = embedding_functions.DefaultEmbeddingFunction()
 
 collection = client.get_or_create_collection(
     name="university_knowledge",
